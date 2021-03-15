718197ee1db4c564b74b88aa89e3761c9f430032
Renzo Muzi <38868339+RenzoMuzi@users.noreply.github.com>
2021-03-15 13:37:41 -0300

    Initial commit

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  preset: 'react-native',
+  setupFiles: [
+    '<rootDir>/node_modules/react-native-gesture-handler/jestSetup.js',
+    '<rootDir>/jest.setup.js',
+  ],
+  setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
+};
