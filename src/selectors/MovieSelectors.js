8a097bc488e9ea569216c2bb165d49a2b19f6a9d
Julian <julianpeloz@gmail.com>
2021-03-31 01:47:23 -0300

    TEMP home from API

--- src/selectors/MovieSelectors.js
+++ src/selectors/MovieSelectors.js
@@ -1,9 +1,7 @@
 export const getTrendingMovies = state => {
-  // return state.movies;
   return Object.values(state.movies.trendingMovies);
 };
 
 export const getRecentlyMovies = state => {
-  // return state.movies;
   return Object.values(state.movies.recentlyMovies);
 };
